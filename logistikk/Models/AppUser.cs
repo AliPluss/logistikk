380eb780d4bc84ce7556d8c4107f9608a4fff241
AliPluss <project.django.rest@gmail.com>
2026-09-17 15:47:54 +0200

    Legg til Identity og InMemory-database

---
+++ logistikk/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+
+namespace logistikk.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+
+}
--- logistikk/Program.cs
+++ logistikk/Program.cs
@@ -1,4 +1,10 @@
+
 using Scalar.AspNetCore;
+using logistikk.Data;
+using logistikk.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace logistikk
 {
     public class Program
@@ -12,6 +18,23 @@ namespace logistikk
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("logistikkDb")); // Use InMemoryDatabase for testing purposes
+
+            builder.Services.AddIdentityCore<AppUser>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 1;
+                options.User.RequireUniqueEmail = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
 
             var app = builder.Build();
 
